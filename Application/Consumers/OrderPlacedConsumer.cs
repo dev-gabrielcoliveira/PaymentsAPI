aceecda1cdaef21cfe25ce319e55f1945b055def
Gabriel Oliveira <gabrielcoliveira.developer@gmail.com>
2026-07-11 13:36:55 -0400

    Implementa consumo de pedidos no PaymentsAPI via MassTransit

---
+++ Application/Consumers/OrderPlacedConsumer.cs
@@ -0,0 +1,40 @@
+﻿using FCG.Application.Events;
+using MassTransit;
+
+namespace FCG.PaymentsAPI.Application.Consumers
+{
+    public class OrderPlacedConsumer : IConsumer<OrderPlacedEvent>
+    {
+        private readonly ILogger<OrderPlacedConsumer> _logger;
+
+        public OrderPlacedConsumer(
+            ILogger<OrderPlacedConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<OrderPlacedEvent> context)
+        {
+            var order = context.Message;
+
+            _logger.LogInformation(
+                "[PaymentsAPI] Processando pagamento. Usuário: {UserId}, Jogo: {GameId}, Valor: {Price}",
+                order.UserId,
+                order.GameId,
+                order.Price);
+
+
+            // Simulação de pagamento aprovado
+            var paymentProcessedEvent = new PaymentProcessedEvent
+            {
+                UserId = order.UserId,
+                GameId = order.GameId,
+                Price = order.Price,
+                Status = "Approved"
+            };
+
+
+            await context.Publish(paymentProcessedEvent);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
+using FCG.PaymentsAPI.Application.Consumers;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<OrderPlacedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Payments API Rodando");
