30ea1d8b6b4b6486422a3c7e4fdf5aaac97b6ca8
Gabriel Oliveira <gabrielcoliveira.developer@gmail.com>
2026-07-12 00:45:10 -0400

    configuração docker e kubernates

--- Application/Events/OrderPlacedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FCG.Application.Events
-{
-    public record OrderPlacedEvent
-    {
-        public int UserId { get; init; }
-        public int GameId { get; init; }
-        public decimal Price { get; init; }
-    }
-}
